b109c0a2aa1b289656ec1db01502faff162ff309
Otacílio Neto <otaneto@rogalabs.com>
2019-09-23 20:39:55 -0300

    added vue and vuetify

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-app-bar app color="black" dark>
+      <v-toolbar-title class="headline text-uppercase">
+        <img width="32" src="@/assets/images/github-logo.svg" alt="Github Logo" />
+        <v-spacer />
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: () => ({
+    //
+  }),
+};
+</script>
