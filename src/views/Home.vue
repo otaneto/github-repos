5cb5755d8efd5d9383d074906748c57b40741b48
Otacílio Neto <otaneto@rogalabs.com>
2019-09-23 21:54:05 -0300

    added search field and RepositoryCard component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>Home</div>
+  <v-container fill-height>
+    <v-layout column justify-center align-center>
+      <v-card width="50%">
+        <v-card-text>
+          <v-autocomplete
+            outlined
+            label="Buscar repositório"
+            no-data-text="Não foram encontrados repositórios com termo buscado"
+          />
+          <repository-card />
+        </v-card-text>
+      </v-card>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+import RepositoryCard from '@/components/RepositoryCard.vue';
 
 export default {
+  name: 'Home',
+  components: {
+    RepositoryCard,
+  },
 };
 </script>
