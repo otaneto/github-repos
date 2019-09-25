d50edac66f1d20daf567a32fc094152026e37e10
Otacílio Neto <otaneto@rogalabs.com>
2019-09-24 23:09:31 -0300

    added tags function

--- src/services/github-service.js
+++ src/services/github-service.js
@@ -4,4 +4,7 @@ export default {
   search(query) {
     return axios.get(`https://api.github.com/search/repositories?q=${query}`);
   },
+  tags(repoName) {
+    return axios.get(`https://api.github.com/repos/${repoName}/tags`);
+  },
 };
