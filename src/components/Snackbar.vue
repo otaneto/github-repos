f04e471005c5cbfbbe5476fbe9f4bdbc430bd28f
Otacílio Neto <otaneto@rogalabs.com>
2019-09-25 01:38:12 -0300

    created Snackbar component

---
+++ src/components/Snackbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-snackbar
+    v-model="snackbar.show"
+    :color="snackbar.color"
+    :top="snackbar.top"
+    :bottom="snackbar.bottom"
+    :left="snackbar.left"
+    :right="snackbar.right"
+    multi-line
+  >
+  <span>
+    <v-icon v-show="!!snackbar.icon" color="white" class="mr-3">{{snackbar.icon}}</v-icon>
+    {{ snackbar.message }}
+  </span>
+  </v-snackbar>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  name: 'snackbar',
+  computed: {
+    ...mapGetters('base', ['snackbar']),
+  },
+  methods: {
+    ...mapActions('base', ['setSnackbar']),
+  },
+  watch: {
+    snackbar(val) {
+      if (val.show) {
+        setTimeout(() => {
+          this.setSnackbar({ show: false });
+        }, 6000);
+      }
+    },
+  },
+};
+</script>
