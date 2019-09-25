09e6206057c8d046c433280a52743895ada25384
Otacílio Neto <otaneto@rogalabs.com>
2019-09-25 00:51:10 -0300

    added real properties to RepositoryCard component

--- src/components/RepositoryCard.vue
+++ src/components/RepositoryCard.vue
@@ -2,48 +2,42 @@
   <v-card elevation="0" round>
     <v-card-text>
       <!-- Main Info -->
-      <v-layout>
-        <a class="title repo-name" href="http://github.com">
-          repo/user
-        </a>
-        <v-spacer />
-        <span class="ml-5">
-          <v-icon size="12">mdi-circle</v-icon>
-          Language
-        </span>
-        <span class="ml-3">
-          <v-icon size="12">mdi-star</v-icon>
-          122k
-        </span>
+      <v-layout align-center>
+        <v-avatar @click="showOwner" class="mr-3 clickable">
+          <img
+            :title="repo.owner.login"
+            :src="repo.owner.avatar_url"
+            alt="Owner Avatar"
+          />
+        </v-avatar>
+
+        <v-layout column>
+          <v-layout>
+            <a class="title repo-name" :href="repo.html_url">{{ repo.full_name }}</a>
+            <v-spacer />
+            <span v-if="repo.language" class="ml-5">
+              <v-icon :color="languageColor" size="12">mdi-circle</v-icon>
+              {{ repo.language }}
+            </span>
+            <span class="ml-3">
+              <v-icon size="12">mdi-star</v-icon>
+              {{ repo.stargazers_count }}
+            </span>
+          </v-layout>
+          <span
+            class="grey--text text-truncate repo-description"
+            :title="repo.description"
+          >
+            {{ repo.description }}
+          </span>
+        </v-layout>
       </v-layout>
 
-      <!-- Secondary Info -->
+      <!-- Details -->
       <v-layout column>
-        <!-- Description -->
-        <span class="grey--text">
-          A declarative, efficient, and flexible JavaScript library for building user interfaces.
-        </span>
-        <!-- Topics -->
         <v-layout class="mt-3">
-          <v-btn
-            class="ml-0 mr-2 text-lowercase blue--text text--darken-2 elevation-0"
-            small
-            color="blue lighten-5"
-          >
-            webpack
-          </v-btn>
-          <v-btn
-            class="ml-0 mr-2 text-lowercase blue--text text--darken-2 elevation-0"
-            small
-            color="blue lighten-5"
-          >
-            javascript
-          </v-btn>
-        </v-layout>
-        <!-- Details -->
-        <v-layout class="mt-3">
-          <span class="mr-4">MIT License</span>
-          <span class="mr-4">MIT License</span>
+          <span v-if="repo.license" class="mr-4">{{ repo.license.name }}</span>
+          <span class="mr-4">Updated {{ updatedAt }}</span>
         </v-layout>
       </v-layout>
     </v-card-text>
@@ -51,13 +45,40 @@
 </template>
 
 <script>
+import githubColors from 'github-colors';
+import { formatDistance } from 'date-fns';
+
 export default {
   name: 'RepositoryCard',
+  props: {
+    repo: {
+      required: true,
+      type: Object,
+      default: () => {},
+    },
+  },
+  computed: {
+    languageColor() {
+      return githubColors.get(this.repo.language) ? githubColors.get(this.repo.language).color : '';
+    },
+    updatedAt() {
+      return formatDistance(new Date(this.repo.updated_at), new Date());
+    },
+  },
+  methods: {
+    formatDistance,
+    showOwner() {
+      window.open(this.repo.owner.html_url);
+    },
+  },
 };
 </script>
 
 <style>
   .repo-name {
     text-decoration: unset;
+  }
+  .repo-description {
+    width: 75%;
   }
 </style>
