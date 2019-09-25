891e6d46ef24dc850c809c944d4c0ea257adcace
Otacílio Neto <otaneto@rogalabs.com>
2019-09-25 01:37:57 -0300

    added base module

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import base from '@/store/modules/base';
 import github from '@/store/modules/github';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
+    base,
     github,
   },
 });
