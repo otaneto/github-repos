1b117fd809c4bad0ddd5f8a06173ef728d87d854
Otacílio Neto <otaneto@rogalabs.com>
2019-09-24 22:14:33 -0300

    organized child components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,48 @@
 <template>
   <v-container fill-height>
     <v-layout column justify-center align-center>
-      <v-card width="50%">
+      <v-card width="50%" height="550">
         <v-card-text>
-          <v-autocomplete
+          <v-text-field
+            v-model="search"
             outlined
-            label="Buscar repositório"
-            no-data-text="Não foram encontrados repositórios com termo buscado"
+            :append-icon="search.length > 0 ? 'mdi-close': ''"
+            @click:append="onClear"
+            :loading="loading"
+            label="Busca"
+            placeholder="Digite o nome do repositório..."
           />
-          <repository-card />
+          <v-container
+            v-if="repos.length === 0 && search.length < 3"
+            class="initial-content"
+            fill-height
+          >
+            <v-layout
+              column
+              align-center
+              justify-center
+            >
+              <img
+                width="200" src="@/assets/images/octocat.png"
+                alt="Logo Github"
+              />
+              <span class="mt-3 headline text-center">
+                Busque seus repositórios favoritos do Github digitando o nome no campo acima.
+              </span>
+            </v-layout>
+          </v-container>
+          
+          <searching-content v-if="loading" />
+          
+          <div
+            :class="{'cards-container': repos.length >= 3}"
+            v-if="repos.length > 0 && !loading"
+          >
+            <repository-card v-for="repo in repos" :key="repo.id" />
+          </div>
+
+          <not-found v-else-if="repos.length === 0 && search.length > 3 && !loading" />
+
         </v-card-text>
       </v-card>
     </v-layout>
@@ -16,12 +50,64 @@
 </template>
 
 <script>
+import githubService from '@/services/github-service';
 import RepositoryCard from '@/components/RepositoryCard.vue';
+import NotFound from '@/components/NotFound.vue';
+import SearchingContent from '@/components/SearchingContent.vue';
 
 export default {
   name: 'Home',
   components: {
+    NotFound,
     RepositoryCard,
+    SearchingContent,
+  },
+  data() {
+    return {
+      repos: [],
+      loading: false,
+      timeout: null,
+      selectedRepo: {},
+      search: '',
+    };
+  },
+  methods: {
+    searchRepos(query) {
+      this.loading = true;
+      clearTimeout(this.timeout);
+      this.timeout = setTimeout(() => {
+        githubService.search(query)
+          .then(({ data }) => {
+            this.repos = data.items;
+          })
+          .finally(() => {
+            this.loading = false;
+          });
+      }, 500);
+    },
+    onClear() {
+      this.search = '';
+    },
+  },
+  watch: {
+    search(val) {
+      if (val.length >= 3) {
+        this.searchRepos(val);
+      }
+    },
   },
 };
 </script>
+
+<style>
+  .cards-container {
+    overflow-y: scroll;
+    max-height: 420px;
+  }
+  .initial-content {
+    height: 430px;
+  }
+  .searching-content {
+    height: 430px;
+  }
+</style>
